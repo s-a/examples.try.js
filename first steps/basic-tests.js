8f8f15b433ebaf556ab5d9313cf75901805f5b76
s-a <stephan.ahlf@googlemail.com>
2015-02-05 18:19:38 +0100

    fixed test

--- first steps/basic-tests.js
+++ first steps/basic-tests.js
@@ -1,8 +1,7 @@
 describe("Mission impossible", function() {
   describe("Robot mission solved", function() {
-    it("should be in the home box", function(done) {
-      should.be.true(true);
-      done();
+    it("should be in the home box", function() {
+      robot.sensor.atHome().should.be.true;
     });
   });
 });
