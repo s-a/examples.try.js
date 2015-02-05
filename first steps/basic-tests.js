ede65208b84d8e18f12a4e63cfb003a6dda83ea6
s-a <stephan.ahlf@googlemail.com>
2015-02-05 18:14:37 +0100

    fixed reference

--- first steps/basic-tests.js
+++ first steps/basic-tests.js
@@ -1,7 +1,7 @@
 describe("Mission impossible", function() {
   describe("Robot mission solved", function() {
     it("should be in the home box", function(done) {
-      should.be.true(window.engine.robot.sensor.atHome());
+      should.be.true(robot.sensor.atHome());
       done();
     });
   });
