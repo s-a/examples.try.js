a1d00f7b64f5202391a1560223f1b5128f5b8197
s-a <stephan.ahlf@googlemail.com>
2015-02-05 08:05:05 +0100

    added tests

---
+++ first steps/basic-tests.js
@@ -0,0 +1,27 @@
+describe("Cow", function() {
+  describe("constructor", function() {
+    it("should have a default name", function() {
+      (5).should.be.exactly(5).and.be.above(1);
+    });
+
+    it("should set cow's name if provided", function() {
+      (5).should.be.exactly(5).and.be.above(1);
+    });
+  });
+
+
+});
+
+describe("muuuh", function() {
+  describe("constructor2", function() {
+    it("should have a default name2", function() {
+      (5).should.be.exactly(5).and.be.above(1);
+    });
+
+    it("should set cow's name if provided2", function() {
+      (5).should.be.exactly(5).and.be.above(1);
+    });
+  });
+
+
+});
