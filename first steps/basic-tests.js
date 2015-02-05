d72393ead66e27e687f4489ac4002d558e245381
s-a <stephan.ahlf@googlemail.com>
2015-02-05 18:10:07 +0100

    test

--- first steps/basic-tests.js
+++ first steps/basic-tests.js
@@ -1,7 +1,8 @@
 describe("Mission impossible", function() {
   describe("Robot mission solved", function() {
-    it("should be in the home box", function() {
+    it("should be in the home box", function(done) {
       should.be.true(window.engine.robot.sensor.atHome());
+      done();
     });
   });
 });
