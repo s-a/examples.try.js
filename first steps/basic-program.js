07c50cf69719ba2bee4916c1a552917ec2dae7eb
s-a <stephan.ahlf@googlemail.com>
2015-02-06 17:09:41 +0100

    fixed obsolete method

--- first steps/basic-program.js
+++ first steps/basic-program.js
@@ -1,13 +1,13 @@
 this
   .clearCommands()
-  .do("move", "left")
-  .do("move", "left")
+  .do("turn", "left")
+  .do("turn", "left")
   .do("run", 4)
-  .do("move", "left")
+  .do("turn", "left")
   .do("run", 4)
-  .do("move", "left")
+  .do("turn", "left")
   .do("run", 9)
-  .do("move", "left")
+  .do("turn", "left")
   .do("run", 8)
   .do("shutdown")
   .start();
