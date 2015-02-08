3c944fb111214d832cb6d8b383563f02047a44b9
s-a <stephan.ahlf@googlemail.com>
2015-02-08 15:17:07 +0100

    basic energy test

--- first steps/basic-tests.js
+++ first steps/basic-tests.js
@@ -1,8 +1,10 @@
 describe("Mission impossible", function() {
   describe("Robot mission solved", function() {
     it("should be in the home box", function() {
-      robot.sensor.sensor.energy.should.be.above(10);
       robot.sensor.atHome().should.be.true;
+    });
+    it("should not use all energy", function() {
+      robot.sensor.sensor.energy.should.be.above(10);
     });
   });
 });
