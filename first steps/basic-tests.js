dfdf9bcc72212afd50185020ed32033fc3939453
s-a <stephan.ahlf@googlemail.com>
2015-02-08 15:15:49 +0100

    added energy test

--- first steps/basic-tests.js
+++ first steps/basic-tests.js
@@ -1,6 +1,7 @@
 describe("Mission impossible", function() {
   describe("Robot mission solved", function() {
     it("should be in the home box", function() {
+      robot.sensor.sensor.energy.should.be.above(10);
       robot.sensor.atHome().should.be.true;
     });
   });
