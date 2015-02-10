8c78ec63cae6a2ee04e7d0308e022bc6476ee04a
s-a <stephan.ahlf@googlemail.com>
2015-02-10 16:39:51 +0100

    program tweak

--- first steps/basic-tests.js
+++ first steps/basic-tests.js
@@ -4,7 +4,7 @@ describe("Mission impossible", function() {
       robot.sensor.atHome().should.be.true;
     });
     it("should be charged after shutdown", function() {
-      robot.sensor.energyLevel().should.be.equal(100);
+      robot.sensor.energyLevel().should.be.equal(100); // 100 percent
     });
   });
 });
