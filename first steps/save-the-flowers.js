8ed356ce5ad0cfcf0277fd9727e436313f2b0435
s-a <stephan.ahlf@googlemail.com>
2015-02-11 07:22:58 +0100

    added new example

---
+++ first steps/save-the-flowers.js
@@ -0,0 +1,45 @@
+this.
+  do("run", 23).
+  do("turn", "left").
+  do("run", 7).
+  do("get").
+
+  do("turn", "around").
+  do("run", 18).
+  do("get").
+
+  do("turn", "right").
+  do("run", 6).
+  do("get").
+
+  do("turn", "right").
+  do("step", "left").
+  do("run", 17).
+  do("get").
+
+  do("turn", "around").
+  do("run", 6).
+  do("turn", "right").
+  do("run", 17).
+  do("charge").
+  do("run", 20).
+  do("put").
+  
+  do("move", "backward").
+  do("move", "backward").
+  do("put").
+  
+  do("move", "backward").
+  do("move", "backward").
+  do("put").
+  
+  do("move", "backward").
+  do("move", "backward").
+  do("put").
+  
+  do("move", "backward").
+  do("turn", "around").
+  do("run", 14).
+  do("charge").
+  do("shutdown").
+  start(); 
