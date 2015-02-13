d50e231b36ee4eefe053e3d8cb53cb7ad00e0ffb
s-a <stephan.ahlf@googlemail.com>
2015-02-13 09:12:39 +0100

    added energy stations demo

---
+++ first steps/low-energy.js
@@ -0,0 +1,9 @@
+this
+  .do("run", 11)
+  .do("charge")
+  .do("run", 11)
+  .do("charge")
+  .do("run", 11)
+  .do("charge")
+  .do("shutdown")
+  .start();
---
+++ first steps/low-energy__tests.js
@@ -0,0 +1,10 @@
+describe("Mission impossible", function() {
+  describe("Robot mission solved", function() {
+    it("should be in the home box", function() {
+      robot.sensor.atHome().should.be.true;
+    });
+    it("should be charged after shutdown", function() {
+      robot.sensor.energyLevel().should.be.equal(100); // 100 percent
+    });
+  });
+});
